f10fcced34f66c8f404de2c1c32cfb91d639bb44
pvictor1206 <paulomagalhaes1206@gmail.com>
2025-04-13 23:17:16 -0300

    assets

--- scripts/characters/test/character_body_3d.gd
+++ scripts/characters/test/character_body_3d.gd
@@ -3,7 +3,7 @@ extends CharacterBody3D
 
 const SPEED = 200.0
 const JUMP_VELOCITY = 10.0
-@onready var animator = get_node("gobot/AnimationPlayer") as AnimationPlayer
+@onready var animator = get_node("sophia/AnimationPlayer") as AnimationPlayer
 
 @export var view : Node3D
 var gravity = 0
@@ -22,11 +22,11 @@ func _physics_process(delta: float) -> void:
 	
 	velocity = applied_velocity
 	
-	move_and_slide()
-	
 	if Vector2(velocity.z, velocity.x).length() > 0:
 		rotacion_direction = Vector2(velocity.z, velocity.x).angle()
 	rotation.y = lerp_angle(rotation.y, rotacion_direction, delta * 10)
+	
+	move_and_slide()
 
 func handle_input(delta):
 	var input := Vector3.ZERO
@@ -38,8 +38,9 @@ func handle_input(delta):
 	velocity = input * SPEED * delta
 	
 func handle_animations():
+	
 	if is_on_floor():
-		if abs(velocity.x) > 1 or abs(velocity.y) > 1:
+		if abs(velocity.x) > 1 or abs(velocity.z) > 1:
 			animator.play("Run", 0.3)
 		else:
 			animator.play("Idle",0.3)

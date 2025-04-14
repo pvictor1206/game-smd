43a657ef2f3e493851fea59699696fdd540cab5f
pvictor1206 <paulomagalhaes1206@gmail.com>
2025-04-13 23:42:55 -0300

    fisica do item

---
+++ scripts/itens/area_soda.gd
@@ -0,0 +1,23 @@
+extends Area3D
+
+const ROTATION_SPEED := 50.0
+
+var start_pos := position.y
+var end_pos := position.y + 0.5
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+		var soda_tween := create_tween().set_loops().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
+		soda_tween.tween_property(self, "position:y", end_pos, 1.0).from(start_pos)
+		soda_tween.tween_property(self, "position:y", start_pos, 1.0).from(end_pos)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	rotate_y(deg_to_rad(ROTATION_SPEED * delta))
+
+
+func _on_body_entered(body: Node3D) -> void:
+	if body.name == "player":
+		#adicionar os codigos aqui
+		queue_free()
